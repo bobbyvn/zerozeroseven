53bd75fa759c090755c1c1314d89423236a47d28
baonguyen091281 <nlvbao@yahoo.com>
2016-08-07 00:27:56 +0700

    init

---
+++ Zero2Seven/Zero2Seven/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zero2Seven.Startup))]
+namespace Zero2Seven
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
